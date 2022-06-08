431e87847a4a94fd7678d5a782d121019f8acecd
Mary Stankevich <marrycitty@mail.ru>
2022-06-08 20:20:01 +0200

    Update ScannerMain.java

--- src/main/java/pingwit/lec_3/hm/task_1pingwit2/ScannerMain.java
+++ src/main/java/pingwit/lec_3/hm/task_1pingwit2/ScannerMain.java
@@ -4,6 +4,7 @@ package pingwit.lec_3.hm.task_1pingwit2;
 import java.util.Arrays;
 import java.util.Scanner;
 
+// +- код не отформатирован
 public class ScannerMain {
         public static void main(String[] args) {
             int[] array = new int[3];
@@ -14,16 +15,18 @@ public class ScannerMain {
                 Scanner scanner = new Scanner(System.in);
                 System.out.println("Enter any number: ");
                 array[i] = scanner.nextInt(); // fullfillin an array w user entered
-                smallestOne = array[0];
+                smallestOne = array[0]; // на каждой итерации будет добавлять одно и то же значение в переменную - может быть вынесено после цикла
 
             }
 
             System.out.println(Arrays.toString(array)); // printin an array fullfilled by the user
+            //проверь этот код на одинаковых наборах чисел в разной последовательности. если результат каждый раз разный - программа работает неверно
             for (i = 1; i < array.length; i++) {
-                if (smallestOne > array.length)
+                if (smallestOne > array.length) //сравнение значения с размером массива - не связано с проверкой, какой элемент минимальный
                     smallestOne = array[i];
-                else biggestOne = array[i];
+                else biggestOne = array[i]; 
             }
+            //в задании указано было еще вывести сумму всех
             System.out.println("The smallest number is: " + smallestOne);
             System.out.println("The biggest number is: " + biggestOne);
         }
