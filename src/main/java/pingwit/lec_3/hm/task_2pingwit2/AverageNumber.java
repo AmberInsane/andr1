a7d45751b1c194153f73fdf52055d1e671dfa83b
Mary Stankevich <marrycitty@mail.ru>
2022-06-08 20:31:53 +0200

    Update AverageNumber.java

--- src/main/java/pingwit/lec_3/hm/task_2pingwit2/AverageNumber.java
+++ src/main/java/pingwit/lec_3/hm/task_2pingwit2/AverageNumber.java
@@ -2,6 +2,7 @@ package pingwit.lec_3.hm.task_2pingwit2;
 
 import java.util.Arrays;
 
+//+ Код не отформатирован
 public class AverageNumber {
     public static void main(String[] args) {
         int[] array = new int[10];
@@ -10,7 +11,8 @@ public class AverageNumber {
         for ( int i = 0; i < array.length; i++) {
             array[i] = i + 3;
             sumOfAllNumbers = sumOfAllNumbers + array[i];
-            average = sumOfAllNumbers / array.length;
+            average = sumOfAllNumbers / array.length; //average лучше вынести после цикла, так как нас интересует только последнее значение, когда сумма всех подсчитана
+                                                      //сейчас считаются промежуточные значения, которые не используются
         }
         System.out.println(Arrays.toString(array));
         System.out.println(sumOfAllNumbers);
